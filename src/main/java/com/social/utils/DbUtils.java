1deeedfb0086ed46364185e89c0a7392b2a35976
Asaf <asaf>
2026-01-30 12:21:15 +0200

    Added Dockerfile

--- src/main/java/com/social/utils/DbUtils.java
+++ src/main/java/com/social/utils/DbUtils.java
@@ -20,16 +20,15 @@ public class DbUtils {
     @PostConstruct
     public void init() {
         try {
-            String host = "localhost";
-            String username = "root";
-            String password = "1234";
-            String schema = "socialnetwork";
-            int port = 3306;
-            String url = "jdbc:mysql://"
-                    + host + ":" + port + "/"
-                    + schema;
-            this.connection =
-                    DriverManager.getConnection(url, username, password);
+            String host = System.getenv("DB_HOST") != null ? System.getenv("DB_HOST") : "localhost";
+            String username = System.getenv("DB_USER") != null ? System.getenv("DB_USER") : "root";
+            String password = System.getenv("DB_PASS") != null ? System.getenv("DB_PASS") : "1234";
+            String schema = System.getenv("DB_NAME") != null ? System.getenv("DB_NAME") : "socialnetwork";
+            String portStr = System.getenv("DB_PORT") != null ? System.getenv("DB_PORT") : "3306";
+            int port = Integer.parseInt(portStr);
+
+            String url = "jdbc:mysql://" + host + ":" + port + "/" + schema;
+            this.connection = DriverManager.getConnection(url, username, password);
             System.out.println("Connection established");
         } catch (SQLException e) {
             System.out.println("Failed to create db connection");
